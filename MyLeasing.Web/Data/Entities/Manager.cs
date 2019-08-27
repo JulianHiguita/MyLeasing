82347d7c500d9648d6d2990ec0a476f55864f2e5
JULIAN ANDRES HIGUITA BRAN <julianhiguita224308@correo.itm.edu.co>
2019-08-27 12:30:11 -0500

    Add user entities

--- MyLeasing.Web/Data/Entities/Manager.cs
+++ MyLeasing.Web/Data/Entities/Manager.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MyLeasing.Web.Data.Entities
 {
-    public class Lessee
+    public class Manager
     {
         public int Id { get; set; }
 
         public User User { get; set; }
 
-        public ICollection<Contract> Contracts { get; set; }
-       
     }
 }
